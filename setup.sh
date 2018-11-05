de920ec9dc532cf9117c5d9a5c5b0ed90f5741f1
Markus Westerlind <marwes91@gmail.com>
2018-11-05 22:42:44 +0100

    Some fixes for docker install and rust

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eux
 
+cp -r .config .tmux.conf ~/
+
 git config --global user.name "Markus Westerlind"
 EMAIL=$1
 git config --global user.email ${EMAIL}
@@ -23,17 +25,17 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-apt-get install docker-ce
+sudo apt-get install docker-ce
+sudo usermod -a -G docker $USER
 pip3 install --user docker-compose
 
 
 which rustup || (curl https://sh.rustup.rs -sSf | sh && \
+    . $HOME/.cargo/env && \
     rustup component add rustfmt-preview rls-preview &&
     rustup install nightly && \
     rustup component add --toolchain nightly rustfmt-preview rls-preview &&
     cargo install cargo-watch ripgrep)
-
-cp -r .config .tmux.conf ~/
 
 if [ ! -f ~/.config/nvim]; then
     (cd ~/.config/ && git clone https://github.com/Marwes/vim-config nvim)
