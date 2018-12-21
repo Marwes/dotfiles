72c19c6afcf40b7bcf51ed5d7df1015e62a0210e
Markus Westerlind <marwes91@gmail.com>
2018-12-21 10:39:45 +0100

    Auto install docker

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce -y
 sudo usermod -a -G docker $USER
 pip3 install --user docker-compose
 
