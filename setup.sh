aaafc1aedb43c3b0073f78b0bc704ed5adcfebf6
Markus Westerlind <marwes91@gmail.com>
2020-06-26 09:04:52 +0200

    Clone the vim config from ssh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ install_nvim() {
     if [ ! -d ~/.config/nvim ]; then
         (mkdir -p ~/.config && \
             cd ~/.config/ && \
-            git clone https://github.com/Marwes/vim-config nvim)
+            git clone git@github.com:Marwes/vim-config nvim)
 
         curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
